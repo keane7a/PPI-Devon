1dd57e273a948e1baa8ba438c3ccb7462077461e
keane7a <keane7a@gmail.com>
2024-06-25 15:13:03 +0100

    move ourteam data into separate file

---
+++ app/about/ourteam/data.ts
@@ -0,0 +1,113 @@
+import { Content } from "@/app/about/ourteam/types"
+
+export const contents: Content[] = [
+    {year: 2023, 
+    committees: 
+    [
+        {
+            image: "", 
+            name: "John", 
+            role: "President", 
+            course: "BSc English", 
+            description: "Hello world, this is a description of the person. you can say anyting",
+            linkedin: "", 
+
+        }, 
+        {
+            image: "", 
+            name: "Doe", 
+            role: "Vice President", 
+            course: "BSc History", 
+            description: "Hello world, this is a description of the person. you can say anyting",
+            linkedin: "", 
+            instagram: "", 
+            facebook: "https://www.facebook.com/groups/171899451981/", 
+        }, 
+        {
+            image: "", 
+            name: "John", 
+            role: "President", 
+            course: "BSc English", 
+            description: "Hello world, this is a description of the person. you can say anyting",
+            linkedin: "", 
+            instagram: "", 
+            facebook: "https://www.facebook.com/groups/171899451981/", 
+        }, 
+        {
+            image: "", 
+            name: "Doe", 
+            role: "Vice President", 
+            course: "BSc History", 
+            description: "Hello world, this is a description of the person. you can say anyting",
+            linkedin: "", 
+            instagram: "", 
+            facebook: "https://www.facebook.com/groups/171899451981/", 
+        }, 
+        {
+            image: "", 
+            name: "John", 
+            role: "President", 
+            course: "BSc English", 
+            description: "Hello world, this is a description of the person. you can say anyting",
+            linkedin: "", 
+
+        }, 
+        {
+            image: "", 
+            name: "Doe", 
+            role: "Vice President", 
+            course: "BSc History", 
+            description: "Hello world, this is a description of the person. you can say anyting",
+            linkedin: "", 
+            instagram: "", 
+            facebook: "https://www.facebook.com/groups/171899451981/", 
+        }, 
+        {
+            image: "", 
+            name: "John", 
+            role: "President", 
+            course: "BSc English", 
+            description: "Hello world, this is a description of the person. you can say anyting",
+            linkedin: "", 
+            instagram: "", 
+            facebook: "https://www.facebook.com/groups/171899451981/", 
+        }, 
+        {
+            image: "", 
+            name: "Doe", 
+            role: "Vice President", 
+            course: "BSc History", 
+            description: "Hello world, this is a description of the person. you can say anyting",
+            linkedin: "", 
+            instagram: "", 
+            facebook: "https://www.facebook.com/groups/171899451981/", 
+        }, 
+    ]
+    },
+    {year: 2024, 
+        committees: 
+        [
+            {
+                image: "", 
+                name: "John", 
+                role: "President", 
+                course: "BSc English", 
+                description: "Hello world, this is a description of the person. you can say anyting",
+                linkedin: "", 
+                instagram: "", 
+                facebook: "", 
+            }, 
+            {
+                image: "", 
+                name: "Doe", 
+                role: "Vice President", 
+                course: "BSc History", 
+                description: "Hello world, this is a description of the person. you can say anyting",
+                linkedin: "", 
+                instagram: "", 
+                facebook: "", 
+            }, 
+        ]
+        },
+
+]
