4bc0899b10652a193ba89a839cee187616e68abd
keane7a <keane7a@gmail.com>
2024-06-26 15:26:36 +0100

    add FAQs to resources

---
+++ app/resources/page.tsx
@@ -0,0 +1,44 @@
+import { Accordion } from "@/app/ui/accordion";
+
+
+var contents = [
+    {
+        title: "hello world", 
+        content: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Nisi eaque mollitia quibusdam, voluptatem quos laboriosam dicta ab culpa illum. Beatae illo dolor ratione dicta, nesciunt maxime nobis quaerat doloribus velit?"
+    },
+    {
+        title: "hello world", 
+        content: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Nisi eaque mollitia quibusdam, voluptatem quos laboriosam dicta ab culpa illum. Beatae illo dolor ratione dicta, nesciunt maxime nobis quaerat doloribus velit?"
+    },
+    {
+        title: "hello world", 
+        content: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Nisi eaque mollitia quibusdam, voluptatem quos laboriosam dicta ab culpa illum. Beatae illo dolor ratione dicta, nesciunt maxime nobis quaerat doloribus velit?"
+    },
+    {
+        title: "hello world", 
+        content: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Nisi eaque mollitia quibusdam, voluptatem quos laboriosam dicta ab culpa illum. Beatae illo dolor ratione dicta, nesciunt maxime nobis quaerat doloribus velit?"
+    },
+    {
+        title: "hello world", 
+        content: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Nisi eaque mollitia quibusdam, voluptatem quos laboriosam dicta ab culpa illum. Beatae illo dolor ratione dicta, nesciunt maxime nobis quaerat doloribus velit?"
+    },
+]
+
+
+export default async function Page() {
+
+    return (
+        <>
+        <h1 className="font-bold text-xl md:text-6xl w-full text-center">FAQs</h1>
+        <div className="rounded-lg bg-gray-100 mt-8 m-4">
+                {contents.map((content) => {         
+                    return (
+                        <>
+                        <Accordion title={content.title} content={content.content} />
+                        </>
+                    );
+                })}
+            </div>
+        </>
+    );
+}
