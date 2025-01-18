38a9576c01f2bc285682393b2ed72e8538b1959c
keane <keane7a@gmail.com>
2025-01-18 15:00:18 +0000

    added more FAQs

--- app/resources/data.tsx
+++ app/resources/data.tsx
@@ -1,27 +1,27 @@
 
 function Link({text, link}: {text: string, link:string})  {
     return (<>
-    <a href={link} className="text-blue-600 dark:text-blue-500 hover:underline">{text}</a>
+    <a href={link} className="text-blue-600 dark:text-blue-500 hover:underline" target="_blank">{text}</a>
     </>)
 }
 
 export var resources = [
     {
         name: "Guidebook 2024", 
-        link: "",
+        link: "/resources/test.pdf",
     }, 
     {
         name: "Anggaran Dasar dan Anggaran Rumah Tangga (AD/ART) 2024", 
         link: "",
     },
-    {
-        name: "Guidebook 2023", 
-        link: "",
-    }, 
-    {
-        name: "Anggaran Dasar dan Anggaran Rumah Tangga (AD/ART) 2023", 
-        link: "",
-    },
+    // {
+    //     name: "Guidebook 2023", 
+    //     link: "",
+    // }, 
+    // {
+    //     name: "Anggaran Dasar dan Anggaran Rumah Tangga (AD/ART) 2023", 
+    //     link: "",
+    // },
 ]
 
 
@@ -31,11 +31,10 @@ export var faqs = [
         answer: 
         <>
         "Once you arrive in the UK there are several public transportations you can choose from such as trains or buses. You can easily book your travels from Trainline or National Express. More information can be found <Link text="here" link="https://www.exeter.ac.uk/students/international/newstudents/planningyourtraveltouniversity/"/>
-        </>,
-        
+        </>  
     },
     {
-        question: "How do I get to Uni from my acommodation", 
+        question: "How do I get to the University from my acommodation?", 
         answer: 
         <>
         You can either walk (most acommodation will take 5-30 minutes to go to uni) or you can take the bus with a service UNI. More information can be found here <Link text="here" link="https://www.stagecoachbus.com/promos-and-offers/south-west/getting-to-exeter-university-by-bus"/>
@@ -44,15 +43,19 @@ export var faqs = [
     {
         question: "What options do I have for acommodation?", 
         answer: <>
-        "There are many! Year 1 students mostly stay on university acommodation located around campus. More information can be found here <Link text="here" link="https://www.exeter.ac.uk/study/accommodation/"/>
+        There are many! Year 1 students mostly stay on university acommodation located around campus. More information can be found here <Link text="here" link="https://www.exeter.ac.uk/study/accommodation/"/>
         </>
     },
     {
-        question: "hello world", 
-        answer: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Nisi eaque mollitia quibusdam, voluptatem quos laboriosam dicta ab culpa illum. Beatae illo dolor ratione dicta, nesciunt maxime nobis quaerat doloribus velit?"
+        question: "Nightlife and going out", 
+        answer: <>
+        Despite being a small city with low population, Exeter has a lively nightlife. You can see more information <Link text="here" link="https://www.exeter.ac.uk/about/thesouthwest/exeter/nightlifeandgoingout/"/>  
+        </>
     },
     {
-        question: "hello world", 
-        answer: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Nisi eaque mollitia quibusdam, voluptatem quos laboriosam dicta ab culpa illum. Beatae illo dolor ratione dicta, nesciunt maxime nobis quaerat doloribus velit?"
-    },
+        question: "How do I ask general inquiries to the the University?",
+        answer: <>
+        University of Exeter has this thing called Student Information Desk (SID) where you can ask anything related to accommodations, fees, international student support, and more! Check it out <Link text="here" link="https://www.exeter.ac.uk/students/sid/"/>  
+        </>
+    }, 
 ]
