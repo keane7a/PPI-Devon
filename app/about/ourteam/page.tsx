9f24c407c31c97eac095776eb1e6ab57e57fad60
keane <keane7a@gmail.com>
2025-02-07 13:57:05 +0000

    fix ProfileCard image size

--- app/about/ourteam/page.tsx
+++ app/about/ourteam/page.tsx
@@ -24,7 +24,7 @@ export default async function Page() {
         <>
         <div className="w-full items-center">
                 <h1 className="m-auto font-bold text-xl md:text-6xl w-full text-center mb-4">Our Team</h1>
-                <p className="text-l text-justify md:text-center w-3/4 md:w-1/2 md:py-4 m-auto" >
+                <p className="text-l text-center md:text-center w-3/4 md:w-1/2 md:py-4 m-auto" >
                     Being aware of the committees in your organisation is crucial to 
                     comprehending the responsibilities they play in directing the organisation&apos;s 
                     leadership structure. This data makes it easier to collaborate and communicate 
